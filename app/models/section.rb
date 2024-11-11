2dce85bce1ed6e50f7896e3c0a032688e5216322
Oleh <Uemail@mail.com>
2024-11-11 15:57:09 +0100

    refactored sections data

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,12 +5,12 @@ class Section < ApplicationRecord
   belongs_to :teacher
   belongs_to :subject
 
-  WORKING_DAYS = [[], [1, 3, 5], [2, 4], [1, 2, 3, 4, 5]].freeze
+  WORKING_DAYS = [[1, 3, 5], [2, 4], [1, 2, 3, 4, 5]].freeze
 
   validates :work_day, :duration, :start_lesson, :teacher_id, :subject_id, :classroom_id, presence: true
   validates :teacher, uniqueness: {scope: [:subject, :classroom]}
   validates :duration, inclusion: { in: [ 50, 80 ] }
-  validates :work_day, numericality: { in: 0..3 }
+  validates :work_day, numericality: { in: 0..2 }
   validates :start_lesson, numericality: { in: 450..1270 }
 
   def end_lesson
